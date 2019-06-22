b776a976434a72ec9f7d64032ac1cf793ae11932
xiaofei.yan <xiaofei.yan@tongdun.cn>
2019-06-22 15:10:14 +0800

    feat: 增加原型设计模式

---
+++ src/java/prototype/Main.java
@@ -0,0 +1,23 @@
+package prototype;
+
+public class Main {
+    public static void main(String[] args) {
+        // 准
+        Manager manager = new Manager();
+        UnderlinePen upen = new UnderlinePen('~');
+        MessageBox mbox = new MessageBox('x');
+        MessageBox sbox = new MessageBox('/');
+        manager.register("strong message", upen);
+        manager.register("warning box", mbox);
+        manager.register("slash box", sbox);
+
+        Product p1 = manager.create("strong message");
+        p1.use("Hello, world.");
+
+        Product p2 = manager.create("warning box");
+        p2.use("Hello, world.");
+
+        Product p3 = manager.create("slash box");
+        p3.use("Hello, world.");
+    }
+}
